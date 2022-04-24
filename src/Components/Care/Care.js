a5a6b34b3f38b4ee2307a7fb074bb7cfa6aab822
Katie T <68283157+KATIETOLER@users.noreply.github.com>
2022-04-24 16:26:13 -0600

    Add care ids for testing

--- src/Components/Care/Care.js
+++ src/Components/Care/Care.js
@@ -4,30 +4,30 @@ import './Care.css'
 const Care = () => {
 	return (
 		<div className='care'>
-			<h4>Food</h4>
+			<h4 className='food'>Food</h4>
 			<p>
 				Feed 1-2 days, and allow your monster to digest its meal. Expect to feed
 				more often if your monster is particularly large or has more red
-				coloring.{' '}
+				coloring.
 				<div>
 					Pro tip: Never ever ever ever feed your monster more than 9 times in a
 					day; <p className='overfeed'>it can be fatal.</p>
 				</div>
 			</p>
-			<h4>Water</h4>
+			<h4 className='water'>Water</h4>
 			<p>
 				Water every 1-2 weeks, allowing them to to dry out between waterings.
 				Expect to water more often in brighter light and less often in lower
 				light. Pro tip: Monsters can benefit from filtered water or water left
 				out overnight before using.
 			</p>
-			<h4>Humidity</h4>
+			<h4 className='humidity'>Humidity</h4>
 			<p>
 				Normal room humidity will do, but prefers humid conditions if possible.
 				Consider incorporating a fine-mist mister or humidifier to boost
 				humidity level indoors.
 			</p>
-			<h4>Temperature</h4>
+			<h4 className='temp'>Temperature</h4>
 			<p>
 				Most monsters prefer temps in the 65°F-85°F range (18°C-30°C). It is
 				best not to let it go below 60°F (15°C). Most monster lovers find that
